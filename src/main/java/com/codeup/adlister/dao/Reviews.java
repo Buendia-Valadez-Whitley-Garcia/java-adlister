b9077ccb73bf84372b94733a9042a94daffa5d25
Savanna Marie <savannamarie@gmail.com>
2021-07-16 10:38:47 -0500

    Reviews dao

--- src/main/java/com/codeup/adlister/dao/Reviews.java
+++ src/main/java/com/codeup/adlister/dao/Reviews.java
@@ -2,10 +2,12 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.Game;
 import com.codeup.adlister.models.Review;
+import com.codeup.adlister.models.User;
 
 import java.util.List;
 
 public interface Reviews {
     List<Review> all(Game game);
-//    Long findReviews(Reviews reviews);
+    Long insert(Review review);
+    List<Review> all(User user);
 }
--- src/main/java/com/codeup/adlister/models/Review.java
+++ src/main/java/com/codeup/adlister/models/Review.java
@@ -2,23 +2,23 @@ package com.codeup.adlister.models;
 
 public class Review {
     private long game_id;
+    private long user_id;
     private String title;
     private String review;
 
 
-    public Review(long game_id, String title, String review) {
-        this.game_id = game_id;
+    public Review(long user_id, String title, String review) {
+        this.user_id = user_id;
         this.title = title;
         this.review = review;
     }
 
 
     public long getId() {
-        return game_id;
+        return user_id;
     }
-
-    public void setGame_id(long game_id) {
-        this.game_id = game_id;
+    public long getGameId() {
+        return game_id;
     }
 
     public void setTitle(String title) {
